61f9e5dcdb3d968f046a15a0443a4152616dd9c6
Rashmi Sarode <rashmisarode92@gmail.com>
2019-12-03 22:54:35 -0800

    image comparison api done

--- routes/photoUpload.js
+++ routes/photoUpload.js
@@ -28,48 +28,145 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+const rekognition = new AWS.Rekognition({
+  apiVersion: '2016-06-27',
+  region: "us-west-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 
 function deleteFileTemp(path) {
-    try {
-      fs.unlinkSync(path)
-      //file removed
-    } catch (err) {
-      console.error("Error in deleting temp file " + path, err);
-    }
+  try {
+    fs.unlinkSync(path)
+    //file removed
+  } catch (err) {
+    console.error("Error in deleting temp file " + path, err);
   }
-  
-  router.post('/upload_photo', function (req, res) {
-    console.log(req.files);
-    console.log(req.body);
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
+}
+
+router.post('/upload_photo', function (req, res) {
+  console.log(req.files);
+  console.log(req.body);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  /* console.log(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_ACCESS_KEY) */
+
+  console.log("tempFilePath:" + req.files.inputFile.tempFilePath)
+  console.log("EventId:" + req.body.eventId)
+
+  const fileContent = fs.createReadStream(req.files.inputFile.tempFilePath);
+  console.log("mimetype: " + req.files.inputFile.mimetype)
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: "cloudhwbucket1/" + req.body.eventId,
+    Key: req.files.inputFile.name,
+    ContentType: req.files.inputFile.mimetype,
+    Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      console.log("Error in uploading file", err);
+      return res.status(500).send(`Can not upload the file. ${err}`)
+      // Send 500 Response 
+    } else {
+      deleteFileTemp(req.files.inputFile.tempFilePath);
+      console.log(`File uploaded successfully. ${data.Location}`);
+
+      return res.status(200).send(`File uploaded successfully. ${data.Location}`)
     }
-    /* console.log(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_ACCESS_KEY) */
-  
-    console.log("tempFilePath:" + req.files.inputFile.tempFilePath)
-    console.log("EventId:" + req.body.eventId)
-    const fileContent = fs.createReadStream(req.files.inputFile.tempFilePath);
-    console.log("mimetype: " + req.files.inputFile.mimetype)
-    // Setting up S3 upload parameters
-    const params = {
-      Bucket: "cloudhwbucket1/"+req.body.eventId,
-      Key: req.files.inputFile.name,
-      ContentType: req.files.inputFile.mimetype,
-      Body: fileContent
-    };
-  
-    // Uploading files to the bucket
-    s3.upload(params, function (err, data) {
-      if (err) {
-        console.log("Error in uploading file", err);
-        return res.status(500).send(`Can not upload the file. ${err}`)
-        // Send 500 Response 
-      } else {
-        deleteFileTemp(req.files.inputFile.tempFilePath);
-        console.log(`File uploaded successfully. ${data.Location}`);
-        return res.status(200).send(`File uploaded successfully. ${data.Location}`)
-      }
-    });
+  });
 });
+
+router.get('/getFaceComparison', function (req, res) {
+  console.log("tempFileName " + req.query.tempFileName);
+  console.log("eventId " + req.query.eventId);
+
+  if (!req.query.eventId) {
+    return res.status(400).send(`Event ID is required`)
+  }
+  if (!req.query.tempFileName) {
+    return res.status(400).send(`tempFileName is required`)
+  }
+
+  const params = {
+    Bucket: "cloudhwbucket1",
+    Prefix: req.query.eventId,
+  };
+
+  s3.listObjects(params, function (err, data) {
+    if (err) {
+      console.log("err in list objects:", err);
+      return res.status(500).send(`List S3 Failed ${err}`);
+    }
+
+    var matched = false;
+
+    data.Contents.forEach(
+      obj => {
+        console.log(obj);
+
+        const tempImage = "temp/"+req.query.tempFileName;
+        compareImages(tempImage, obj.Key, (result)=> {
+        console.log("Compared : ",tempImage, "with ",obj.Key, "result:"+result)
+        if (result) {
+          matched = true;
+          return res.status(200).send("matched");
+        } 
+       });
+      }
+    );
+
+    setTimeout(()=> {
+      if(!matched) {
+        return res.status(403).send("unmatched");
+      }
+    }, 5000);
+
+  });
+
+});
+
+function compareImages(image1, image2, callBackFunc) {
+  var params = {
+    SimilarityThreshold: 90,
+    SourceImage: {
+      S3Object: {
+        Bucket: "cloudhwbucket1",
+        Name: image1
+      }
+    },
+    TargetImage: {
+      S3Object: {
+        Bucket: "cloudhwbucket1",
+        Name: image2
+      }
+    }
+  };
+  rekognition.compareFaces(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack)
+      callBackFunc(false);
+    } else {
+      console.log("Comparing : ",image1, image2 , data);
+      
+      if (data.FaceMatches && data.FaceMatches.length >0 ) {
+        const result = data.FaceMatches[0];
+        
+        if (result.Similarity >= 90.0) {
+          console.log("Similarity:", result.Similarity);
+          callBackFunc(true);
+        } 
+      } else {
+        callBackFunc(false);
+      }
+    }
+  });
+
+
+}
+
 module.exports = router;
