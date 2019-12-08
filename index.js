839e45c6cf1f1ae2b091e44104b708d5846d1bb6
mayurib2 <bhisemayuri@gmail.com>
2019-12-07 16:48:09 -0800

    Update health api

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api', fileupload);
 app.use('/photoUpload', photoUpload);
 
 app.get('/', function (req, res) {
-    res.send('Hello World from code ninjas')
+    res.send('Hello World from code ninjas!!')
 })
 
 
