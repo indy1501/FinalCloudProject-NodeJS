a35989d559035c5b91d4fa8ab030bae01b6e32c5
shubham05101994 <maverick@Mavericks-MBP.hsd1.ca.comcast.net>
2019-12-09 12:24:16 -0800

    Trigger jenkins build

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api', fileupload);
 app.use('/photoUpload', photoUpload);
 
 app.get('/', function (req, res) {
-    res.send('Hello World!!!')
+    res.send('We are team Code ninjas')
 })
 
 
